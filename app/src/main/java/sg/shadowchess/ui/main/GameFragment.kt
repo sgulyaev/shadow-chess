2a0d14a5bbc973440d8da27f5a546d8a7ae2d75c
sgulyaev <sgulyaev@gmail.com>
2020-11-10 23:19:59 +0300

    by viewModels

--- app/src/main/java/sg/shadowchess/ui/main/GameFragment.kt
+++ app/src/main/java/sg/shadowchess/ui/main/GameFragment.kt
@@ -1,19 +1,15 @@
 package sg.shadowchess.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import sg.shadowchess.R
 
-class MainFragment : Fragment() {
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
-    private lateinit var viewModel: MainViewModel
+class GameFragment : Fragment() {
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
@@ -21,7 +17,6 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.board
     }
 
